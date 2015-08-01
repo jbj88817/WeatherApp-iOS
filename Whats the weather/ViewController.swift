04e4883bed4fc1aaae5418927d7641678fcba602
Bojie Jiang <jiangbojie@gmail.com>
2015-08-01 11:44:09 -0700

    Layout and get web content

--- Whats the weather/ViewController.swift
+++ Whats the weather/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    
+    @IBOutlet weak var displayWeatherLabel: UILabel!
+    
+    @IBAction func getWeatherBtn(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string:"http://www.weather-forecast.com/locations/San-Francisco/forecasts/latest")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                print(webContent)
+            }
+        }
+        
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
