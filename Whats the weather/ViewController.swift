8c2c1a231ece68192b9bd6010d80e876d46fc787
Bojie Jiang <jiangbojie@gmail.com>
2015-07-31 23:04:21 -0700

    Initial Commit

---
+++ Whats the weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats the weather
+//
+//  Created by Bojie Jiang on 7/31/15.
+//  Copyright © 2015 Bojie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
